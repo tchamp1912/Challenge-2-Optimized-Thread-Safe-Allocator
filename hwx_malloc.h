7d3abbca26885cb44fdaf0ad59b45c8e73a99094
Unnas Hussain <unnas.w.hussain@medtronic.com>
2020-03-24 22:04:51 -0400

    thread-safe working w unnas's allocator

--- hwx_malloc.h
+++ hwx_malloc.h
@@ -4,6 +4,23 @@
 // Husky Malloc Interface
 // cs3650 Starter Code
 
+/////////////////////////////////////////////////////////////////////
+////////////////////////////// llist.h //////////////////////////////
+
+// based on generic linked list code and lecture code
+
+typedef struct llist_node {
+    size_t size;     // size of one block
+    struct llist_node* next; // NULL if end of list
+} llist_node;
+
+long llist_length(llist_node* list_head);
+llist_node* llist_insert(llist_node* to_insert, llist_node* list_head);
+// void print_list(llist_node* list_head); // was giving clang errors and wasn't needed
+
+/////////////////////////////////////////////////////////////////////
+////////////////////////////// hmalloc.h //////////////////////////////
+
 typedef struct hm_stats {
     long pages_mapped;
     long pages_unmapped;
@@ -12,19 +29,13 @@ typedef struct hm_stats {
     long free_length;
 } hm_stats;
 
-typedef struct node_t {
-	size_t size;
-	struct node_t *next;
-} node_t;
-
-typedef struct header_t {
-	size_t size;
-} header_t;
-
 hm_stats* hgetstats();
 void hprintstats();
+void* xmalloc(size_t size);
+void xfree(void* item);
+llist_node* xmallocHlp_get_free_block(size_t min_size);
 
-void* hmalloc(size_t size);
-void hfree(void* item);
+
 
 #endif
+
